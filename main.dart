cef0ede62c8181df3de5dbc1adef4a576c085b09
오윤진 <87515295+dhdbsrlw@users.noreply.github.com>
2023-03-03 17:15:21 +0900

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('I Am YJ'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+/*
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.purple[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+        ),
+      ),
+  );
+}
+*/
